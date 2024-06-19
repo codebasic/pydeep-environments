abb00084a91f4457d0a9ccb450d5efa746d504d6
Lee Seongjoo <seongjoo@codebasic.io>
2024-06-19 15:18:51 +0900

    Tensorflow=2.16, Keras=3.3

--- apple_silicon.sh
+++ apple_silicon.sh
@@ -8,7 +8,7 @@ if [ -z "$VENV_NAME" ]; then
 fi
 echo "파이썬 환경 명칭: $VENV_NAME"
 
-conda create -y -n $VENV_NAME -c conda-forge python=3.9
+conda create -y -n $VENV_NAME -c conda-forge python=3.10
 echo "NumPy 설치 (BLAS 가속을 위해 빌드)"
 # gcc 설치 확인
 if ! command -v gcc &> /dev/null
@@ -27,7 +27,7 @@ echo "Tensorflow 설치"
 conda run -n $VENV_NAME pip install tensorflow
 conda run -n $VENV_NAME pip install tensorflow-metal
 conda run -n $VENV_NAME pip install keras-tuner
-conda run -n $VENV_NAME pip install keras
+conda run -n $VENV_NAME pip install keras~=3.3.0
 echo
 echo "데이터 분석 패키지 설치"
 conda run -n $VENV_NAME pip install scikit-learn 
